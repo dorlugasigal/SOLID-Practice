5f05f1537f4d07161a1174732a46e1d1da797b02
Dor Lugasi-Gal <dorlugasigal@gmail.comgit>
2019-05-23 23:17:56 +0300

    lets try to push from viusl studio

--- OCPLibrary/Accounts.cs
+++ OCPLibrary/Accounts.cs
@@ -5,8 +5,9 @@
         public EmployeeModel Create(PersonModel person)
         {
             EmployeeModel output = new EmployeeModel();
-            output.FirstName = person.FirstName;
             output.LastName = person.LastName;
+            output.FirstName = person.FirstName;
+
             output.EmailAddress = $"{person.FirstName.Substring(0, 1)}{person.LastName}@gmail.com";
             
             return output;
